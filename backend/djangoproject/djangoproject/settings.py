c3c7128b24f69f7e3efb49ed5c33f65b58ec5e2e
K.Nacer-eddine <k.nacereddine1@gmail.com>
2024-01-11 22:56:00 +0100

    Integration of the backend with the frontend in the static folder and connection to MySQL db

--- backend/djangoproject/djangoproject/settings.py
+++ backend/djangoproject/djangoproject/settings.py
@@ -75,8 +75,15 @@ WSGI_APPLICATION = 'djangoproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'bizquiz',
+        'USER': 'root',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '3306',
+        'OPTIONS': {
+            'sql_mode': 'traditional',
+        }
     }
 }
 
