a97fc13ef75d30c13940c24ea266be56d32b5792
K.Nacer-eddine <k.nacereddine1@gmail.com>
2024-01-12 18:32:53 +0100

    declared the models in admin.py file and migrate them to my database and created a superuser to manage my data

--- backend/djangoproject/djangoapp/admin.py
+++ backend/djangoproject/djangoapp/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
 # Register your models here.
+
+from .models import CustomUser, Quiz, QuizScore, Question, Answer, AnswerSubmission
+
+admin.site.register(CustomUser)
+admin.site.register(Quiz)
+admin.site.register(QuizScore)
+admin.site.register(Question)
+admin.site.register(Answer)
+admin.site.register(AnswerSubmission)
