7a3a2dd6f9593cf43e285f0bf5d3dd2a13f72c79
K.Nacer-eddine <k.nacereddine1@gmail.com>
2024-01-12 13:36:09 +0100

    add models and installed Pillow to handle ImageField and configure urls.py and settings.py

--- backend/djangoproject/djangoapp/models.py
+++ backend/djangoproject/djangoapp/models.py
@@ -1,3 +1,53 @@
+from django.contrib.auth.models import AbstractUser, Group, Permission
 from django.db import models
+from django.utils.translation import gettext_lazy as _
 
-# Create your models here.
+class CustomUser(AbstractUser):
+    profile_picture = models.ImageField(upload_to='profile_pics/', null=True, blank=True)
+    total_user_score = models.IntegerField(default=0)
+    groups = models.ManyToManyField(Group, verbose_name=_('groups'), blank=True, related_name='custom_user_groups')
+    user_permissions = models.ManyToManyField(Permission, verbose_name=_('user permissions'), blank=True, related_name='custom_user_user_permissions')
+
+    def __str__(self):
+        return self.username
+
+class Quiz(models.Model):
+    title = models.CharField(max_length=255)
+    difficulty = models.CharField(max_length=20)
+    description = models.TextField()
+
+    def __str__(self):
+        return self.title
+
+class QuizScore(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='quiz_scores')
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='quiz_scores')
+    score = models.IntegerField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.quiz.title}"
+
+class Question(models.Model):
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE)
+    text = models.TextField()
+
+    def __str__(self):
+        return self.text
+
+class Answer(models.Model):
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+    is_correct = models.BooleanField(default=False)
+
+    def __str__(self):
+        return self.text
+
+class AnswerSubmission(models.Model):
+    user = models.ForeignKey(CustomUser, on_delete=models.CASCADE, related_name='answer_submissions')
+    quiz = models.ForeignKey(Quiz, on_delete=models.CASCADE, related_name='answer_submissions')
+    question = models.ForeignKey(Question, on_delete=models.CASCADE, related_name='answer_submissions')
+    selected_answer = models.ForeignKey(Answer, on_delete=models.CASCADE, related_name='answer_submissions')
+    is_correct = models.BooleanField()
+
+    def __str__(self):
+        return f"{self.user.username} - {self.question.text} - {self.selected_answer.text} ({'Correct' if self.is_correct else 'Incorrect'})"
--- backend/djangoproject/djangoproject/settings.py
+++ backend/djangoproject/djangoproject/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'djangoapp.apps.DjangoappConfig',
 ]
 
 MIDDLEWARE = [
@@ -128,3 +129,7 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+MEDIA_ROOT = BASE_DIR / "media"
+
+MEDIA_URL = '/media/'
