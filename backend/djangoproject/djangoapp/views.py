3b9d4dcc1e09701121f9a4fcf29d836bdeb3e487
K.Nacer-eddine <k.nacereddine1@gmail.com>
2024-01-23 22:53:34 +0100

    Added the serializers and developes the register user logic

--- backend/djangoproject/djangoapp/views.py
+++ backend/djangoproject/djangoapp/views.py
@@ -1,4 +1,13 @@
 from django.shortcuts import render
 from rest_framework.views import APIView
+from rest_framework.response import Response
+from .serializers import CustomUserSerializer
+
 
 # Create your views here.
+class RegisterView(APIView):
+    def post(self, request):
+        serializer = CustomUserSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.save()
+        return Response(serializer.data)
