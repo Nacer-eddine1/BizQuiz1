2d583049fd332d0354758412bc3e78cc36d870c8
K.Nacer-eddine <k.nacereddine1@gmail.com>
2024-01-27 21:53:58 +0100

    Generated the home, loging and sign-up components and dectared them in the app module

--- frontend/bizquiz/src/app/app.module.ts
+++ frontend/bizquiz/src/app/app.module.ts
@@ -6,12 +6,18 @@ import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home/home.component';
+import { LoginComponent } from './login/login.component';
+import { SignUpComponent } from './sign-up/sign-up.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    LoginComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
