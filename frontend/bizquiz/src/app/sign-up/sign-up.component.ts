4ed00bc722a10ac202a1103e32cf851e872dca57
maskmohammed <154214372+maskmohammed@users.noreply.github.com>
2024-02-07 14:47:41 +0100

    password conditions first commit

--- frontend/bizquiz/src/app/sign-up/sign-up.component.ts
+++ frontend/bizquiz/src/app/sign-up/sign-up.component.ts
@@ -1,10 +1,14 @@
 import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
-import { FormBuilder, FormGroup } from '@angular/forms';
+import { AbstractControl, FormBuilder, FormGroup, PatternValidator, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { faFacebookF } from '@fortawesome/free-brands-svg-icons';
+import { faCancel, faCheckCircle } from '@fortawesome/free-solid-svg-icons';
 import { faGooglePlusG } from '@fortawesome/free-brands-svg-icons';
 import { faLinkedinIn } from '@fortawesome/free-brands-svg-icons';
+import { PatternValidatorsService } from '../shared/services/patternValidators/pattern-validators.service';
+import { ConfirmPasswordService } from '../shared/services/confirmPassword/confirm-password.service';
+// import {confirmpass}
 
 @Component({
   selector: 'app-sign-up',
@@ -23,8 +27,39 @@ export class SignUpComponent implements OnInit {
   ngOnInit(): void {
     this.form = this.formBuilder.group({
       username: '',
-      email: '',
-      password: '',
+      email: ['', [Validators.required, Validators.email]],
+      password: [
+        '',
+        [
+          Validators.compose(
+            [
+              Validators.required,
+              PatternValidatorsService.patternValidators(/\d/, {hasNumber:true}),
+              PatternValidatorsService.patternValidators(/[A-Z]/, {hasCapitalCase:true}),
+              PatternValidatorsService.patternValidators(/[a-z]/, {hasSmallCase:true}),
+              PatternValidatorsService.patternValidators(
+                /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/, {hasSpecialCharacters:true}),
+              Validators.minLength(8),
+            ]
+          )  
+        ]
+      ],
+      confirm_password: [
+        '',
+        [
+          Validators.compose(
+            [
+              Validators.required,
+              PatternValidatorsService.patternValidators(/\d/, {hasNumber:true}),
+              PatternValidatorsService.patternValidators(/[A-Z]/, {hasCapitalCase:true}),
+              PatternValidatorsService.patternValidators(/[a-z]/, {hasSmallCase:true}),
+              PatternValidatorsService.patternValidators(
+                /[ !@#$%^&*()_+\-=\[\]{};':"\\|,.<>\/?]/, {hasSpeacalCharacters:true}),
+              Validators.minLength(8),
+            ]
+          )  
+        ]
+      ]
     });
   }
   signup(): void {
@@ -32,7 +67,30 @@ export class SignUpComponent implements OnInit {
       .post('http://localhost:8000/api/register', this.form.getRawValue())
       .subscribe(() => this.router.navigate(['/login']));
   }
+
+  get username(){
+    return this.form.get('username')
+  }
+
+  get email(){
+    return this.form.get('email')
+  }
+
+  get password(){
+    return this.form.get('password')
+  }
+
+  get confirmpassword(){
+    return this.form.get('confirm_password')
+  }
+
   faFacebookF = faFacebookF;
   faLinkedinIn = faLinkedinIn;
   faGooglePlusG = faGooglePlusG;
+  faCancel = faCancel;
+  faCheckCircle = faCheckCircle;
+
+// service confirm password
+  
 }
+
